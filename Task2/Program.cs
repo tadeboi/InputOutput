5d04cdcbfd55758e9e9f603e853138a9cbe3caf1
Omotade Alabi <omotade.alabi@thebulb.africa>
2020-12-16 14:26:17 +0100

    Added an Exception

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -9,22 +9,30 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            string filePath = "C:\\New folder\\test.txt";
-
-            int count = 0;
-            List<string> status = new List<string>();
-
-            for (int i = 0; i < 5; i++)
+            try
             {
-                count++;
-                Console.WriteLine("Input your current status at {0}:", DateTime.Now);
-                string current = Console.ReadLine();
-                Thread.Sleep(5000);
+                string filePath = "C:\\New folder\\test.txt";
 
-                status.Add(current+" at "+ DateTime.Now);
+                int count = 0;
+                List<string> status = new List<string>();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    count++;
+                    Console.WriteLine("Input your current status at {0}:", DateTime.Now);
+                    string current = Console.ReadLine();
+                    Thread.Sleep(5000);
+
+                    status.Add(current + " at " + DateTime.Now);
+                }
+                status.Reverse();
+                File.AppendAllLines(filePath, status);
             }
-            status.Reverse();
-            File.AppendAllLines(filePath, status);
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
