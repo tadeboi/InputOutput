afb60ec7b5c3fb39ee45f003ae33de6b1c7f55b0
Omotade Alabi <omotade.alabi@thebulb.africa>
2020-12-16 14:09:49 +0100

    Add project files.

---
+++ InputOutput/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Input your Username: ");
+                string username = Console.ReadLine();
+                Console.WriteLine("Input your Filename: ");
+                string filename = Console.ReadLine();
+                Console.WriteLine("Type in your blog content: ");
+                string content = Console.ReadLine();
+
+                string filePath = "C:\\New folder\\" + filename + ".html";
+         
+                //using (FileStream fs = File.Create(filePath))
+                //{
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+
+                File.AppendAllText(filePath, "<h1 style='text-align:center;'>Welcome to " + username + "'s blog!</h1>");
+                File.AppendAllText(filePath, "<p style='text-align:center;'>" + content + "</p>");
+
+                //}
+                //using (StreamWriter str = File.CreateText(filePath))
+                //{
+                //    //str.WriteLine("This is from me");
+                //    //str.WriteLine("This is amazing");
+                //    str.WriteLine("<h1>Welcome to {0}'s blog!</h1>",username);
+                //    str.WriteLine("<p>{0}</p>",content);
+                //    str.Close();
+                //}
+                ////ile.AppendAllText(path, "This is an appended line");
+                ////File.WriteAllLines(path, "");
+                //File.ReadAllText(filePath);
+
+                //LowLevelWrite(filePath);
+                var innerText = File.ReadAllText(filePath);
+
+                //Console.WriteLine(innerText);
+                Console.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+            
+        }
+        static void LowLevelWrite(string path)
+        {
+            //var str = new StreamWriter(path);
+            //str.WriteLine("This is from me");
+            //str.WriteLine("This is amazing");
+            using (var str = new StreamWriter(path))
+            {
+                //str.WriteLine("This is from me");
+                //str.WriteLine("This is amazing");
+                str.WriteLine("<h1>Hello Daniel!</h1>");
+                str.Close();
+            }
+            //ile.AppendAllText(path, "This is an appended line");
+            //File.WriteAllLines(path, "");
+            File.ReadAllText(path);
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\New folder\\test.txt";
+
+            int count = 0;
+            List<string> status = new List<string>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                count++;
+                Console.WriteLine("Input your current status at {0}:", DateTime.Now);
+                string current = Console.ReadLine();
+                Thread.Sleep(5000);
+
+                status.Add(current+" at "+ DateTime.Now);
+            }
+            status.Reverse();
+            File.AppendAllLines(filePath, status);
+        }
+    }
+}
